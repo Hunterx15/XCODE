286c25e7bd854637369e942ba31fdaecd7a37bdf
Pankaj Jangid <pgjangid15@gmail.com>
2025-12-24 16:38:07 +0530

    final changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,12 +13,11 @@ const cors = require("cors");
 
 // console.log("Hello")
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+
 
 app.use(express.json());
 app.use(cookieParser());
