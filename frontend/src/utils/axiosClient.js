c1177d8ec11e49ab099064dbbc6146c53dc5a9fe
Pankaj Jangid <pgjangid15@gmail.com>
2026-01-07 08:33:06 +0530

    dist file added

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // e.g. https://xcode-uxcs.onrender.com
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000', // e.g. https://xcode-uxcs.onrender.com
   withCredentials: true,                 // ⭐ REQUIRED for cookies
   headers: {
     "Content-Type": "application/json",
